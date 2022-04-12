3e08827e1ed2c72673f35ccc7142d792c75719ce
shaimasharin <shaimasharin1@gmail.com>
2022-04-12 12:34:42 +0530

    mapping

---
+++ src/components/CardData.js
@@ -0,0 +1,25 @@
+let cardData = [
+    {
+    imgSrc:
+            "https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80",
+          name: "Web Diary",
+          phone: 45566565,
+          email: "webdiary@gmail.com"
+    },
+
+    {
+          imgSrc:"https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+          name: "code Diary",
+          phone: 55566565,
+          email: "codediary@gmail.com"
+    },
+
+    {
+        imgSrc:"https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+        name: "dev Diary",
+        phone: 66666565,
+        email: "devdiary@gmail.com"
+  },
+];
+
+export default cardData; 
